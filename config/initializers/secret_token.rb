840323d8cfbe7d2eb47eb83faf797c7aeac2469a
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-07-26 15:17:54 +1200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otjru::Application.config.secret_token = '4c9185f93a0622b3767d937b099d996ea3310cdb9c98213f938eb1cea3abbac30e6437d02427dec770bca29528396254bb2d6cfbc9e83a6a5bfc4259c7547812'
