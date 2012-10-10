0ac40c7fa4fce698f75857dbd991a3d424be2dc1
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-10-10 17:26:19 +1300

    mission hub is working

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Otjru::Application.initialize!
-
-Missionhub.config do |c|
-  c.base_uri = 'http://www.missionhub.com/'
-  c.client_id = 6
-  c.client_secret = '05fbcedcf2b2ea3958b359b51796b14b1c04c19a'
-  c.org_id = 6775
-end
--- lib/MissionHub.rb
+++ lib/MissionHub.rb
@@ -7,8 +7,11 @@ module Missionhub
 
   attr_accessor :client_id, :client_secret, :base_uri, :org_id
 
-  # And we define a wrapper for the configuration block, that we'll use to set up
-  # our set of options
+  base_uri = 'http://www.missionhub.com/'
+  client_id = 6
+  client_secret = '05fbcedcf2b2ea3958b359b51796b14b1c04c19a'
+  org_id = 6775
+
   def config(&block)
     instance_eval(&block)
   end
